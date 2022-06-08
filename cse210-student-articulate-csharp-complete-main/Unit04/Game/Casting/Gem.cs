183eadac96f3fec300f311cb93e80bbbeb221739
Jenna Scholl <bin19005@byui.edu>
2022-06-08 17:01:47 -0600

    saving updates

---
+++ cse210-student-articulate-csharp-complete-main/Unit04/Game/Casting/Gem.cs
@@ -0,0 +1,29 @@
+namespace Unit04.Game.Casting
+{
+    /// <summary>
+    /// <para>An item of cultural or historical interest.</para>
+    /// <para>
+    /// The responsibility of an Artifact is to provide a message about itself.
+    /// </para>
+    /// </summary>
+    public class Gem : Actor
+    {
+        private string message = "";
+
+        /// <summary>
+        /// Constructs a new instance of an Artifact.
+        /// </summary>
+        public Gem()
+        {
+        }
+
+        /// <summary>
+        /// Gets the artifact's message.
+        /// </summary>
+        /// <returns>The message.</returns>
+
+        /// <summary>
+        /// Sets the artifact's message to the given value.
+        /// </summary
+    }
+}
